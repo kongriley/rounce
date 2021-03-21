9e7a07c49c8a06d4ecbb04609edd40d424bb7ab0
Riley Kong <kongriley@gmail.com>
2021-03-20 20:58:19 -0700

    Initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <Nuxt />
+  </div>
+</template>
+
+<style>
+html {
+  font-family:
+    'Source Sans Pro',
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    'Helvetica Neue',
+    Arial,
+    sans-serif;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+    rounce
+
+    Global news, delivered.
+
+    Enter a news site to translate and summarize:
+    (search)
+
+    Or select an existing site:
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
